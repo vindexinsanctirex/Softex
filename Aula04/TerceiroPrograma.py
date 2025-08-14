06677ca322a518e3df45b232601c3ba1eedbd551
Caio S Duarte <caiosduarte@protonmail.com>
2025-08-14 19:36:25 +0000

    atualizado

--- Aula04/TerceiroPrograma.py
+++ Aula04/TerceiroPrograma.py
@@ -1,26 +1,65 @@
-teste = "excelsior"
+# estudo de for e while loops
 
-print(teste.isalpha())  
-print(teste.isdigit())  
-print(teste.isalnum())  
-print(teste.islower())  
-print(teste.isupper())  
-print(teste.isspace())  
-print(teste.capitalize())  
-print(teste.title())  
-print(teste.lower())  
-print(teste.upper())  
-print(teste.strip())  
-print(teste.replace('e', 'b'))  
-print(teste.split())  
-print(teste.join(['a', 'b']))  
-print(teste.count('a'))  
-print(teste.startswith('a'))  
-print(teste.endswith('a'))  
-print(teste.splitlines())  
-print(teste.partition('a'))  
-print(teste.rpartition('a'))  
-print(teste.zfill(5))  
-print(teste.format_map({}))  
-print(teste.removesuffix('a'))  
-print(teste.isnumeric())  
+# for i in range(1, 11):
+#     print(i)
+# print("Fim do Loop")
+# for y in range(10, 1, -1):
+#     print(y)
+# print("Fim do Loop")
+# for i in range(1, 10, 1):
+#     print(i)
+# print("Fim do Loop")
+# for x in range(10):
+#     x+= 1
+#     print(x)
+# print("Fim do Loop")
+# for x in range(10):
+#     print(x + 1)
+
+# for conta in range(100):
+#     conta += 1
+#     print(conta)
+#     if conta == 50:
+#         print("Chegou na metade!")
+#     elif conta == 75:
+#         print("Chegou nos 3/4 do caminho!")
+#     elif conta == 100:
+#         print("Chegou ao final!")
+
+número = int(input("Digite um número: "))
+
+if número % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+media = (nota1 + nota2) / 2
+
+if media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
+
+número1 = int(input("Digite o primeiro número: "))
+número2 = int(input("Digite o segundo número: "))
+if número1 > número2:
+    print(f"{número1} é maior que {número2}.")
+elif número1 < número2:
+    print(f"{número2} é maior que {número1}.")
+else:
+    print("Os números são iguais.")
+
+idade = int(input("Digite sua idade: "))
+if idade < 12:
+    print("Você é uma criança.")
+elif idade < 17:
+    print("Você é um adolescente.")
+elif idade < 64:
+    print("Você é um adulto.")
+else:
+    print("Você é um idoso.")
+
